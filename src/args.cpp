584deabdc3ed591f848f434541e93aee1d21e6a8
SyntaxError2505 <mr.sascha.cherevko@gmail.com>
2025-12-09 18:59:51 +0100

    Finished implementing switch statement in args.cpp

--- src/args.cpp
+++ src/args.cpp
@@ -13,15 +13,18 @@ args::args(int argc, const char* argv[]){
                 std::cout << "Syntax Error. Every argument needs to be followed by a String\n";
             }
 
+            //switch for arguments
             switch(argv[i][1]){
                 case 'n':
                 i++;
                 this->project_name = argv[i];
                 break;
+
                 case 's':
                 i++;
                 this->source_paths.push_back(argv[i]);
                 break;
+
                 case 'i':
                 i++;
                 if(argv[i][1] == 'U'){
@@ -36,6 +39,31 @@ args::args(int argc, const char* argv[]){
                 }
                 this->include_paths.push_back(argv[i]);
                 this->include_paths[this->include_paths.size() - 1].erase(0, 2); //erase PU/PR in a beutifull one-liner
+                break;
+
+                case 'e':
+                i++;
+                this->subdir_paths.push_back(argv[i]);
+                break;
+
+                case 'f':
+                i++;
+                this->packages.push_back(argv[i]);
+                break;
+
+                case 'v':
+                i++;
+                this->packages.push_back(argv[i]);
+                break;
+
+                case 'c':
+                i++;
+                if(sizeof(argv[i]) > 1){
+                    this->cpp = true;
+                }
+                else{
+                    this->cpp = false;
+                }
                 break;
             }
         }
